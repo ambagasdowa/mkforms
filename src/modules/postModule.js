9ead1eebc9dc2b0740bfbf8702d324713aebc3ec
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2023-03-18 11:07:08 -0600

    Update in views presentation v0.0.2

--- src/modules/postModule.js
+++ src/modules/postModule.js
@@ -41,7 +41,7 @@ console.log(`Loading uploadInit module `);
 const log_msg = "Initializing postModule";
 export { log_msg };
 
-function eventOnDom(element, typeEvent, page) {
+function eventOnDom(element, typeEvent, url, page) {
   // |this| is a newly created object
   this.name = `Initialize Dom ${typeEvent}`;
   this.handleEvent = function (event) {
