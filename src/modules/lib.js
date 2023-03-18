cd4d7575218518a33f26cb5e39e5d572d4132998
へすす あるべると Baizabal <ambagasdowa@gmail.com>
2023-03-17 20:39:27 -0600

    Improvements in views v0.0.1

--- src/modules/lib.js
+++ src/modules/lib.js
@@ -297,6 +297,7 @@ function setForms(obj, index, div) {
   button.id = "submit";
   button.value = "Submit";
   button.className = "btn";
+  button.innerText = "Click_Me";
   formx.appendChild(button);
   div.appendChild(formx);
 } // NOTE End setForms
